f09dfbb5b719eaa983571eb23734d60fd2a0bdba
K2H2O <108336278+K2H2O@users.noreply.github.com>
2025-04-20 20:59:03 +0200

    Add files via upload

---
+++ Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StallenbergJCG_Assign1.Views
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
