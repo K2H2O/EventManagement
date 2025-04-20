8ca539854ce2fb15d4aa97d12131dbfcaea9a148
K2H2O <108336278+K2H2O@users.noreply.github.com>
2025-04-20 20:56:12 +0200

    Add files via upload

---
+++ Controllers/AttendeeController.cs
@@ -0,0 +1,58 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using EventManagementSystem.Models;
+using EventManagementSystem.Data;
+using System.ComponentModel.Design;
+
+namespace EventManagementSystem.Controllers
+{
+    public class AttendeeController : Controller
+    {
+        private readonly IEventRepository _eventRepository;
+        public AttendeeController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+        [HttpGet]
+        public IActionResult Register(int eventID)
+        {
+            var eventDetails = _eventRepository.GetEventById(eventID);
+            if (eventDetails == null)
+            {
+                return NotFound();
+            }
+            var registration = new Attendee { EventID = eventID };
+            return View(registration);
+
+        }
+        [HttpPost]
+        public IActionResult Register(Attendee attendee)
+        {
+
+            var Event = _eventRepository.GetEventById(attendee.EventID);
+            if (Event != null)
+            {
+                Event.EventRegistration++;
+            }
+            _eventRepository.AddAttendee(attendee);
+            _eventRepository.SaveChanges();
+
+            return RedirectToAction("Confirmation", new { eventId = attendee.EventID });
+
+        }
+   
+
+        [HttpGet]
+        public IActionResult Confirmation(int eventId)
+        {
+            var myEvent = _eventRepository.GetEventWithAttendees(eventId);
+            if (myEvent == null)
+            {
+                return NotFound();
+            }
+            return View(myEvent);
+        }
+    }
+}
---
+++ Controllers/EventController.cs
@@ -0,0 +1,29 @@
+﻿using EventManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventRepository _eventRepository;
+        public EventController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_eventRepository.GetAllEvents());
+        }
+        public IActionResult Details(int eventID)
+        {
+            var _event = _eventRepository.GetEventById(eventID);
+        
+            if(_event == null)
+            {
+            return NotFound();
+            }
+        return View(_event);
+            }
+    }   
+}
