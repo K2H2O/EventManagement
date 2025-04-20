d970cbaea983d56c114cb5c9c18be73e27ec8a7d
K2H2O <108336278+K2H2O@users.noreply.github.com>
2025-04-20 20:57:11 +0200

    Add files via upload

---
+++ Models/Attendee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementSystem.Models
+{
+    public class Attendee
+    {
+        public int AttendeeID { get; set; }
+
+        [Required(ErrorMessage = "Enter your Name")]
+        [DisplayName("Name")]
+        public string AttendeeName { get; set; }
+        [Required (ErrorMessage = "Enter your E-Mail")]
+        [RegularExpression(".+\\@.+\\..+",  ErrorMessage = "Please Enter your Email")]
+        [DisplayName("E-Mail")]
+        public string AttendeeEmail { get; set; }
+        [Required]
+        public int EventID { get; set;}
+        public Event Event { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementSystem.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        [Required]
+        [DisplayName("Description")]
+        public string EventDescription { get; set; }
+        [Required]
+        [DisplayName("Title")]
+        public string EventTitle { get; set; }
+        [DisplayName("Start Date")]
+        public DateTime EventStartDate { get; set; }
+        [DisplayName("End Date")]
+        public DateTime? EventEndDate { get; set;}
+        [Required]
+        [DisplayName("Location")]
+        public string EventLocation { get; set; }
+
+        [Required]
+        public int EventMaxAttendees { get; set; }
+
+        public int EventRegistration { get; set; }
+        public ICollection<Attendee> Attendees { get; set; }
+    }
+}
